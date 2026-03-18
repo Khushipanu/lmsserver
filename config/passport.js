90096f50f4d07b5dd72fc1b425c25c28ad689a1c
MACBOOK PRO <macbookpro@Khushi.local>
2026-03-18 18:10:57 +0530

    url changed

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL:"https://lmsserver-1.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
